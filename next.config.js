8bd00b45fd6982174c829b050f4852246a9c00aa
Yasaman Ansari <yasamanansari2016@gmail.com>
2021-10-02 15:52:54 +0330

    Remove sentry for now

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 // @ts-check
-
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-const { withSentryConfig } = require('@sentry/nextjs');
-
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
 /**
@@ -22,16 +17,5 @@ const nextConfig = {
 	mode: IS_PRODUCTION ? 'production' : 'development', // Used for tree shaking, see "sideEffects" in package.json
 };
 
-const SentryWebpackPluginOptions = {
-	// Additional config options for the Sentry Webpack plugin. Keep in mind that
-	// the following options are set automatically, and overriding them is not
-	// recommended:
-	//   release, url, org, project, authToken, configFile, stripPrefix,
-	//   urlPrefix, include, ignore
 
-	debug: false,
-	// For all available options, see:
-	// https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-module.exports = withSentryConfig(nextConfig, SentryWebpackPluginOptions);
+module.exports = nextConfig
