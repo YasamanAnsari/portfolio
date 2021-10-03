8c70eb0ca5eeff718d8459826e476d11b7f675c5
Yasaman Ansari <yasamanansari2016@gmail.com>
2021-10-03 19:04:25 +0330

    Make experiences and educations responsive

--- src/ui/molecules/resume-section/educations.tsx
+++ src/ui/molecules/resume-section/educations.tsx
@@ -6,14 +6,18 @@ import type { FC } from 'react';
 
 const { educations } = myResumeSections;
 export const Educations: FC<{ className?: string }> = ({ className }) => (
-	<ul className={['flex', className].filter(Boolean).join(' ')}>
+	<ul
+		className={['flex flex-col md:flex-row', className]
+			.filter(Boolean)
+			.join(' ')}
+	>
 		{educations.list.map((education, index) => (
 			<ResumeSection
 				key={index}
 				name={education.universityName}
 				field={education.field}
 				duration={education.duration}
-				className='w-240px'
+				className='mb-8 last:mb-0 md:mb-0 w-240px'
 			/>
 		))}
 	</ul>
--- src/ui/molecules/resume-section/experiences.tsx
+++ src/ui/molecules/resume-section/experiences.tsx
@@ -6,14 +6,18 @@ import type { FC } from 'react';
 
 const { experiences } = myResumeSections;
 export const Experiences: FC<{ className?: string }> = ({ className }) => (
-	<ul className={['flex', className].filter(Boolean).join(' ')}>
+	<ul
+		className={['flex flex-col md:flex-row', className]
+			.filter(Boolean)
+			.join(' ')}
+	>
 		{experiences.list.map((experience, index) => (
 			<ResumeSection
 				key={index}
 				name={experience.jobTitle}
 				field={experience.companyTitle}
 				duration={experience.duration}
-				className='w-240px'
+				className='mb-8 last:mb-0 w-240px md:mb-0'
 			/>
 		))}
 	</ul>
