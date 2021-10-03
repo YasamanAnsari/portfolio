2996d126d520a294f7fbc7879a764ef0596382f1
Yasaman Ansari <yasamanansari2016@gmail.com>
2021-10-03 15:59:26 +0330

    Create the first version of portfolio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
+import { summeryAboutMe, websiteDescription, websiteTitle } from 'lib/contents';
+import { Socials } from 'ui/molecules/socials';
+
 import type { NextPageWithLayout } from 'next';
 
 const IndexPage: NextPageWithLayout = () => (
 	<>
-		<h1>Hello world</h1>
+		<h1 className='mb-4'>{websiteTitle}</h1>
+		<h2 className='mb-2'>{websiteDescription}</h2>
+		<span className='mb-4'>Connect me:</span>
+		<Socials />
+		<p className='mt-6'>{summeryAboutMe}</p>
 	</>
 );
 
